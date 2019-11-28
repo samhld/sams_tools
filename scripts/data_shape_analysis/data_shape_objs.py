16e3705960e0ed23f1f36d2a8ed815b0a0281bb4
Sam Dillard <sam@influxdata.com>
2019-11-27 20:49:17 -0800

    Fix: fixed bug where lines with escaped spaces were bypassing the parser
    Feat: Added a few basic functions

--- scripts/data_shape_analysis/data_shape_objs.py
+++ scripts/data_shape_analysis/data_shape_objs.py
@@ -5,6 +5,7 @@ import math
 import numpy as np
 import seaborn as sns
 import scipy.stats as stats
+import re
 
 file = 'metrics.out'
 
@@ -16,8 +17,8 @@ class Plotter:
     def __init__(self, text):
         #add error handling for not being passed text (or file if I go that route)
         self.measurements = []
-        self.tags = []
-        self.fields = []
+        self.tags = [[]]
+        self.fields = [[]] # in text with multiple lines, this is a list of lists
         self.timestamps = []
         self._parse(text)
         self.num_lines = sum(1 for line in open(file))
@@ -25,23 +26,52 @@ class Plotter:
         self._fields_dict = { f"Line{i+1} fields": len(elem) for i,elem in enumerate(self.fields)}
 
     def _parse(self,text):
-        for line in text.splitlines():
-            line_tags, line_fields, timestamp = line.split()
-            line_tags = line_tags.split(',')
-            measurement = line_tags.pop(0)
-            self.measurements.append(measurement)
-            line_fields = line_fields.split(',')
-            self.tags.append(line_tags)
-            self.fields.append(line_fields)
-            self.timestamps.append(timestamp)
+
+        try:
+            for line in text.splitlines():
+                # if line.count(' ') == 2:
+                line_tags, line_fields, timestamp = re.split('(?<!\\\\)\s', line)
+                line_tags = line_tags.split(',')
+                measurement = line_tags.pop(0)
+                self.measurements.append(measurement)
+                line_fields = line_fields.split(',')
+                self.tags.append(line_tags)
+                self.fields.append(line_fields)
+                self.timestamps.append(timestamp)
+        except ValueError:
+            print(line)
+
+            # elif line.count(' ') < 2:
+            #     other_lines = []
+            #     other_lines.append(line)
+        #     #     print(other_lines)
+        #     else:
+        #         other_lines = []
+        #         other_lines.append(line)
+        # print(other_lines[0])
+
+            # elif line.count(' ') == 1:
+            #     # counts fields if no tags in line (1 space in line indicates no tags)
+            #     measurement = line.pop(0)
+            #     line_fields, timestamp = line.split()
+            #     line_fields = line_fields.split(',')
+
 
     def get_total_fields(self):
-        _total_fields = len(self._fields_dict)
+        _total_fields = sum(self._fields_dict.values())
         return _total_fields
 
     def get_total_tags(self):
-        _total_tags = len(self._tags_dict)
+        _total_tags = sum(self._tags_dict.values())
         return _total_tags
+
+    def get_max_tags(self):
+        _max_tag = max(self._tags_dict.values())
+        return _max_tag
+
+    def get_max_fields(self):
+        _max_field = max(self._fields_dict.values())
+        return _max_field
 
     def _average(self, _list):
 

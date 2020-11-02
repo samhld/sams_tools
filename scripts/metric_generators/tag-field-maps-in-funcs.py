2d64ac56c5f0abb48b2aa96fb696319cbdd31b19
Sam Dillard <sam@influxdata.com>
2020-11-02 10:40:46 -0800

    complete function definitions for field and tag mappings

--- scripts/metric_generators/tag-field-maps-in-funcs.py
+++ scripts/metric_generators/tag-field-maps-in-funcs.py
@@ -14,17 +14,25 @@ def _gen_int(num):
 
 num_tags = 2
 num_tag_values = 11
-tag_value_size=10
-tag_key_size=10
-int_fields = 2
-str_fields = 1
-float_fields = 1
-field_key_size = 10
-int_value_size = 4
-str_value_size = 10
-float_value_size = 8
+# tag_value_size=10
+# tag_key_size=10
+# int_fields = 2
+# str_fields = 1
+# float_fields = 1
+# field_key_size = 10
+# int_value_size = 4
+# str_value_size = 10
+# float_value_size = 8
 
-num_lines = num_tag_values/num_tags
+num_lines = int(num_tag_values/num_tags)
+
+def _gen_tag_map(num_tags=2,num_tag_values=11,tag_value_size=10,tag_key_size=10):
+
+    # Creates Tag mappings --> number of requested Tags to number of values needed to satisfy
+    tag_keys = ['tag_' + _gen_string(tag_key_size-4) for tag_key in range(num_tags)]
+    tag_values = [_gen_string(tag_value_size) for tag_value in range(num_lines)]
+    tags_map = {k:[v for v in tag_values] for k in tag_keys} # create key-value mapping from lists above
+    print(tags_map)
 
 def _gen_field_map(int_fields=2, 
                     str_fields=1, 
@@ -50,16 +58,16 @@ def _gen_field_map(int_fields=2,
         str_field_values = [_gen_string(str_value_size) for field_value in range(num_lines)]
         
         str_fields_map = {k: [v for v in str_field_values] for k in str_field_keys}
-
+        
         field_map.update(str_fields_map)
     # Creates float field mappings --> number of requested keys to number of values needed to satisfy
     if float_fields:
         float_field_keys = ['fl_' + _gen_string(field_key_size-3) for field_key in range(float_fields)]
         float_field_values = [round(random.uniform(10,99), float_value_size-2) for field_value in range(num_lines)]
-        float_fields_map = {k: [v for v in float_field_values] for k in str_field_keys}
+        float_fields_map = {k: [v for v in float_field_values] for k in float_field_keys}
 
         field_map.update(float_fields_map)
-        print(float_fields_map)
+
     return field_map
 
-print(_gen_field_map())
+print(_gen_field_map(),_gen_tag_map())
